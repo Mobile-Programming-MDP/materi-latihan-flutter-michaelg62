58f6b1c4a78e4a6b2cab1d6e8adf91c4920c2451
Michael Gunawan <michaelgunawan_2226250062@mhs.mdp.ac.id>
2023-11-17 18:22:35 +0700

    Tombol Back dan InkWell

--- wisata_candi/lib/widgets/item_card.dart
+++ wisata_candi/lib/widgets/item_card.dart
@@ -11,7 +11,8 @@ class ItemCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: 6. Implementasi routing ke DetailScreen
-    onTap: () {
+    onTap:
+    () {
       Navigator.push(
         context,
         MaterialPageRoute(
@@ -20,56 +21,66 @@ class ItemCard extends StatelessWidget {
       );
     };
 
-    return Card(
-      // TODO: 2. Tetapkan parameter shape, margin, dan elevation dari Cari
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15), 
-      ),
-      margin: EdgeInsets.all(4),
-      elevation: 1,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // TODO: 3. Buat Image sebagai anak dari Column
-          Expanded(
-            // TODO: 7. Implementasi Hero animation
-            child: ClipRRect(
-              child: Image.asset(
-                candi.imageAsset,
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-              left: 16,
-              top: 8,
-            ),
-            child: Text(
-              candi.name,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetailScreen(candi: candi),
+          )
+        );
+      },
+      child: Card(
+        // TODO: 2. Tetapkan parameter shape, margin, dan elevation dari Cari
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        margin: EdgeInsets.all(4),
+        elevation: 1,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // TODO: 3. Buat Image sebagai anak dari Column
+            Expanded(
+              // TODO: 7. Implementasi Hero animation
+              child: ClipRRect(
+                child: Image.asset(
+                  candi.imageAsset,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(15),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-              left: 16,
-              bottom: 8,
-            ),
-            child: Text(
-              candi.type,
-              style: TextStyle(
-                fontSize: 12,
+            Padding(
+              padding: EdgeInsets.only(
+                left: 16,
+                top: 8,
+              ),
+              child: Text(
+                candi.name,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          // TODO: 4. Buat Text sebagai anak dari Column
-          // TODO: 5. Buat Text sebagai anak dari Column
-        ],
+            Padding(
+              padding: EdgeInsets.only(
+                left: 16,
+                bottom: 8,
+              ),
+              child: Text(
+                candi.type,
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            // TODO: 4. Buat Text sebagai anak dari Column
+            // TODO: 5. Buat Text sebagai anak dari Column
+          ],
+        ),
       ),
     );
   }
